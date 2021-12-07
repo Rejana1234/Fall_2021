d890cafcaf1ce01be34e52258fce89df9d3290e1
Rejana1234 <90631076+Rejana1234@users.noreply.github.com>
2021-12-07 13:07:33 +0600

    Update db.php

--- FTLabExam/db.php
+++ FTLabExam/db.php
@@ -3,7 +3,6 @@
  $dbuser = "root";
  $dbpass = "";
  $db = "mysql";
-    $con = mysqli_connect($dbemployee);
     
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
